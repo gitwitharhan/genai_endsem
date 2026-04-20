eb4d92ce87d03090980413c2aee949e4ed0a4cb0
Tejas <tejas10108@gmail.com>
2026-04-21 00:48:59 +0530

    Fix in predict.py

--- src/model/predict.py
+++ src/model/predict.py
@@ -1,7 +1,7 @@
 import joblib
 import os
 
-# Robust path handling: get the directory of the current script
+# Robust path handling: get the directory of the current script.
 base_path = os.path.dirname(os.path.abspath(__file__))
 # the models directory is two levels up from src/model/
 model_path = os.path.join(base_path, "../../models/ev_demand_model.pkl")
